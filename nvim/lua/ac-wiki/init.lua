da5348f884dd19879bec2c0b4325c78a2cb673bf
acarvalho <abcarvalho@uchicago.edu>
2021-05-19 16:44:01 -0500

    ported wikivim config to lua

--- nvim/lua/ac-wiki/init.lua
+++ nvim/lua/ac-wiki/init.lua
@@ -1,11 +1,12 @@
-vim.g.wiki_root = os.getenv('$ZEN_WIKI_DIR')
-vim.g.wiki_filetypes = 'md'
+vim.g.wiki_root = os.getenv('ZEN_WIKI_DIR')
+vim.g.wiki_filetypes = {'md'}
 vim.g.wiki_link_target_type = 'md'
 vim.g.wiki_link_extension = '.md'
 
-vim.g.wiki_mappings_use_default= 0
-vim.g.wiki_global_load= 0
-vim.g.wiki_link_toggle_on_follow= 0
+vim.g.wiki_mappings_use_default = 0
+vim.g.wiki_global_load = 0
+vim.g.wiki_link_toggle_on_follow = 0
+-- vim.g.wiki_mappings_use_defaults='global'
 -- let g:wiki_mappings_global = {
 --         \ '<plug>(wiki-link-follow)' : '<TAB>',
 --         \}
