947b4ae12d93e0aa97710d341c1080d35dcabd44
Artur Carvalho <abcarvalho@uchicago.edu>
2021-09-16 17:33:28 -0500

    updated treesitter's highlight and indentation options

--- nvim/lua/ac-treesitter/init.lua
+++ nvim/lua/ac-treesitter/init.lua
@@ -1,8 +1,12 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {"bash", "bibtex", "julia", "latex", "lua", "python"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     -- ignore_install = {},
-    highlight = {
-        enable = true -- false will disable the whole extension
-    },
+	highlight = {
+                        enable = true,
+                        language_tree = true
+                    },
+                    indent = {
+                        enable = true
+                    },
     -- indent = {enable = true, disable = {"python", "html", "javascript"}},
 }
