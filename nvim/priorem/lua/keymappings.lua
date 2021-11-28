1621d31a1431ab8358bdb03df4c1c8a277f07a6b
Artur Carvalho <abcarvalho@uchicago.edu>
2021-11-28 17:06:24 -0600

    removed old autocompletion commands

---
+++ nvim/priorem/lua/keymappings.lua
@@ -0,0 +1,7 @@
+
+-- Autocompletion [REMOVE] {{{1
+-- kmap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+-- kmap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+-- kmap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- kmap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- }}}1
