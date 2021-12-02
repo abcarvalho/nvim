11446d32ff1f6e5298915ff91a5779d431bb377a
Artur Carvalho <abcarvalho@uchicago.edu>
2021-12-01 20:47:47 -0600

    minor changes to file formatting

--- lua/ab-lsp/init.lua
+++ lua/ab-lsp/init.lua
@@ -3,10 +3,12 @@
 local nvim_lsp = require('lspconfig')
 -- require('lspconfig').setup() -- important
 
-vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
-vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
-vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
-vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.cmd([[
+  nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>"
+  nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>"
+  nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>"
+  nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>"
+]])
 
 -- symbols for autocomplete
 vim.lsp.protocol.CompletionItemKind = {
