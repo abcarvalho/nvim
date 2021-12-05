414142a62112efc9be8286cc2c434a08d3eba379
Artur Carvalho <abcarvalho@uchicago.edu>
2021-12-05 08:46:40 -0600

    now using jedi language server for python

--- lua/ab-lsp/init.lua
+++ lua/ab-lsp/init.lua
@@ -55,7 +55,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'jedi_language_server', }
+local servers = { 'jedi_language_server', 'julials'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
