b518ac3390e7aa33407419252769f47279090481
Artur Carvalho <abcarvalho@uchicago.edu>
2021-12-05 16:34:33 -0600

    replaced pylsp with jedi language server

--- lua/ab-cmp/init.lua
+++ lua/ab-cmp/init.lua
@@ -61,7 +61,7 @@ local cmp = require'cmp'
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['pylsp'].setup {
+require('lspconfig')['jedi_language_server'].setup {
   capabilities = capabilities
 }
 
