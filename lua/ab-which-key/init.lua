5ec4ca2643a6d6b624e332d1344f78bff1143de8
Artur Carvalho <abcarvalho@uchicago.edu>
2021-12-05 17:26:51 -0600

    updated window keymappings

--- lua/ab-which-key/init.lua
+++ lua/ab-which-key/init.lua
@@ -16,8 +16,10 @@ local leader_mappings = {
 -- single mappings {{{2
    -- j = { '<C-W>s', 'split below'},  -- [ADJUST]
 -- List default and user-defined commands
-   C = {'<cmd>Telescope commands<cr>', 'list commands'}, -- [ADJUST]
-   z = {'<cmd>ZenMode<cr>', 'zen-mode'},
+  C = {'<cmd>Telescope commands<cr>', 'list commands'}, -- [ADJUST]
+  z = {'<cmd>ZenMode<cr>', 'zen-mode'},
+  ['-'] = {'<cmd>split<cr>' , 'split-window-below'},
+  ['|'] = {'<cmd>vs<cr>' , 'split-window-right'},
 -- }}}2
 -- b is for buffers {{{2
   b = {
@@ -203,8 +205,8 @@ local leader_mappings = {
     J = {'<cmd>resize -2<cr>','expand-window-below'},
     L = {'<cmd>vertical resize -2<cr>', 'expand-window-right'},
     K = {'<cmd>resize +2<cr>'     ,'expand-window-up'   },
-    ['0'] = {'<cmd>split<cr>' , 'split-window-below'},
-    ['-'] = {'<cmd>vs<cr>' , 'split-window-right'},
+    ['-'] = {'<cmd>split<cr>' , 'split-window-below'},
+    ['|'] = {'<cmd>vs<cr>' , 'split-window-right'},
     ['='] ={"<C-w>=", 'balance-window'},
     o = {"<cmd>only<cr>", 'close-all-windows-except-current'},
       },
